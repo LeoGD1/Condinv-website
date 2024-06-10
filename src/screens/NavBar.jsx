53b5c05eaf3ac0d4a00b0c4f48e0b52c6d5d8a09
LeoGD1 <bridonegd@outlook.com>
2024-06-09 21:36:02 -0600

    correccion de rutas de archivos de navbar

--- src/screens/NavBar.jsx
+++ src/screens/NavBar.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import '../stylesheets/NavBar.css';
+import logo from '../assets/logo-condinv.svg';
+import iconInstagram from '../assets/icon-instagram.svg';
+import iconWhatsapp from '../assets/icon-whatsapp.svg';
 
 const NavBar = ( {instagram, whatsapp} ) => {
   return (
     <nav className='nav'>
-      <img src='./src/assets/logo-condinv.svg' alt='logo condinv' className='nav__logo'/>
+      <img src={logo} alt='logo condinv' className='nav__logo'/>
       <ul className="nav__ul">
         <li className="nav__li">
             <a href={instagram} className="nav__a">
-                <img src="src/assets/icon-instagram.svg" alt="icono de instagram" className="nav__icon" />
+                <img src={iconInstagram} alt="icono de instagram" className="nav__icon" />
             </a>
         </li>
         <li className="nav__li">
             <a href={whatsapp} className="nav__a">
-                <img src="src/assets/icon-whatsapp.svg" alt="icono de whatsapp" className="nav__icon" />
+                <img src={iconWhatsapp} alt="icono de whatsapp" className="nav__icon" />
             </a>
         </li>
       </ul>
